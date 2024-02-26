ecdf8b683a0216024de604f2953b8d8602130b21
Affeeq Ismail <48659449+Affeeq@users.noreply.github.com>
2024-02-26 13:53:32 -0800

    FO-1934 add public api entry

--- projects/ngx-time-scheduler/src/public_api.ts
+++ projects/ngx-time-scheduler/src/public_api.ts
@@ -2,7 +2,10 @@
  * Public API Surface of ngx-time-scheduler
  */
 
-export * from './lib/ngx-time-scheduler.service';
-export * from './lib/ngx-time-scheduler.component';
-export * from './lib/ngx-time-scheduler.module';
-export * from './lib/ngx-time-scheduler.model';
+export * from "./lib/ngx-time-scheduler.service";
+export * from "./lib/ngx-time-scheduler.component";
+export * from "./lib/ngx-time-scheduler.module";
+export * from "./lib/ngx-time-scheduler.model";
+export * from "./lib/ngx-time-scheduler.enums";
+export * from "./lib/ngx-time-scheduler.constants";
+export * from "./lib/ngx-time-scheduler.utils";
